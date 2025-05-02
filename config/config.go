a206455db48b4054ce90d404b5ea9545f40c0aa8
Ahmet Çevrim <purplecevrim@gmail.com>
2025-05-02 19:15:22 +0300

    update config for deploy

--- config/config.go
+++ config/config.go
@@ -7,10 +7,17 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+// func LoadEnv() {
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
+
+// }
+
+func LoadEnv() { //renderda deploy ettiğim için bu kısmı kullanıyorum.
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, continuing with system environment variables...")
 	}
 }
 
